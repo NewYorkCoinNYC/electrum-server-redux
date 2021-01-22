ccaa24de25a97ecf061a100e9aec680efe5b114f
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2021-01-22 19:48:19 +0000

    Add files via upload

---
+++ src/networks.py
@@ -0,0 +1,14 @@
+# Main network and testnet definitions
+
+params = {
+    'bitcoin_main': {
+        'pubkey_address': 60,
+        'script_address': 22,
+        'genesis_hash': '5597f25c062a3038c7fd815fe46c67dedfcb3c839fbc8e01ed4044540d08fe48'
+    },
+    'bitcoin_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
+        'genesis_hash': '24463e4d3c625b0a9059f309044c2cf0d7e196cf2a6ecce901f24f681be33c8f'
+    }
+}
